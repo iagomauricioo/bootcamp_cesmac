2cde0aad3419a44306ef4bb01f5bcb2f5753a169
Iago <118476701+iagomauricioo@users.noreply.github.com>
2024-01-22 09:13:04 -0300

    Segunda aula - Encapsulamento

---
+++ segunda-aula/src/Carro.java
@@ -0,0 +1,43 @@
+public class Carro {
+    private double tamanho;
+    private String cor;
+    private float altura;
+    private int peso;
+
+    public void acelerar() {}
+    public void frear() {}
+    public void ligar() {}
+    public void desligar() {}
+
+    public double getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(double tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public java.lang.String getCor() {
+        return cor;
+    }
+
+    public void setCor(java.lang.String cor) {
+        this.cor = cor;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+}
